54917cc9895ade502fa988e4096a74fcdc850fef
Peter Peev <peev.petar@gmail.com>
2019-11-16 01:50:46 +0200

    Add project files.

---
+++ StackAndQueues/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var inputFromConsole = Console.ReadLine();
+
+			Stack<char> stack = new Stack<char>();
+
+			foreach (var letter in inputFromConsole)
+			{
+				stack.Push(letter);
+			}
+
+			var iterations = stack.Count;
+
+			for (int i = 0; i < iterations; i++)
+			{
+				Console.Write(stack.Pop());
+			}
+			
+		}
+	}
+}
---
+++ StackSum/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var inputCommands = Console.ReadLine();
+
+			var initalDigits = inputCommands.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+			Stack<int> sumStack = new Stack<int>();
+
+			foreach (var initalDigit in initalDigits)
+			{
+				sumStack.Push(int.Parse(initalDigit));
+			}
+
+			
+
+
+			while (inputCommands.ToLower() != "end")
+			{
+
+				inputCommands = Console.ReadLine();
+
+				string[] inputArr = inputCommands.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+				switch (inputArr[0].ToLower())
+				{
+					case "add":
+						sumStack.Push(int.Parse(inputArr[1]));
+						sumStack.Push(int.Parse(inputArr[2]));
+
+						break;
+					case "remove":
+						if (int.Parse(inputArr[1]) < sumStack.Count)
+						{
+							for (int i = 0; i < int.Parse(inputArr[1]); i++)
+							{
+							
+								sumStack.Pop();
+							
+							}
+						}
+						
+						break;
+
+				}
+
+
+				
+
+			}
+
+			var stackCount = sumStack.Count;
+
+			var resultSum = 0;
+
+			for (int i = 0; i < stackCount; i++)
+			{
+				resultSum += sumStack.Peek();
+				sumStack.Pop();
+
+			}
+			Console.WriteLine($"Sum: {resultSum}");
+		}
+	}
+}
